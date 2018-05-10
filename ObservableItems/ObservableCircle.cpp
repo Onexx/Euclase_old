d2e6eef67aa95d65d6fa1bcf43f8d887cc4c2c1f
Onexx <ilya7538@mail.ru>
2018-05-10 16:28:10 +0300

    V 1.4.0 bug fix. direction change completely works

--- ObservableItems/ObservableCircle.cpp
+++ ObservableItems/ObservableCircle.cpp
@@ -25,12 +25,11 @@ void ObservableCircle::DirectionDrawing(Direction &dir){
     dirline->setPen(QPen(Qt::red, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
     dirline->setData(Qt::UserRole, this->data(Qt::UserRole));
     switch ( dir ){
-    case(Right) : dirline->setLine(this->rect().width(), this->rect().height()/2, this->rect().width()+7, this->rect().height()/2);
-    case(Down)  : dirline->setLine(this->rect().width()/2, 0, this->rect().width()/2, -7);
-    case(Left)  : dirline->setLine(0, this->rect().height()/2, -7, this->rect().height()/2);
-    case(Up)    : dirline->setLine(this->rect().width()/2, this->rect().height(), this->rect().width()/2, this->rect().height()+7);
+    case(Right) : {dirline->setLine(this->rect().width(), this->rect().height()/2, this->rect().width()+7, this->rect().height()/2);break;}
+    case(Down)  : {dirline->setLine(this->rect().width()/2, this->rect().height(), this->rect().width()/2, this->rect().height()+7);break;}
+    case(Left)  : {dirline->setLine(0, this->rect().height()/2, -7, this->rect().height()/2);break;}
+    case(Up)    : {dirline->setLine(this->rect().width()/2, 0, this->rect().width()/2, -7);break;}
     }
-    qDebug()<<this->data(Qt::UserRole)<<' '<<dirline->data(Qt::UserRole)<<' '<<dir;
     scene()->addItem(dirline);
 }
 
@@ -46,6 +45,6 @@ void ObservableCircle::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
          case( Left ) : CurrentDirection = Up;    break;
          case( Up )   : CurrentDirection = Right; break;
         }
+        DirectionDrawing(CurrentDirection);
     }
-    DirectionDrawing(CurrentDirection);
 }
